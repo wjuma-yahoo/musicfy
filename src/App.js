8993c16b638ca4583c910618e34440313bf12062
neoja <wjuma19dev@yahoo.com>
2022-04-09 21:23:03 -0400

    Agregado firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Firebase from './utils/Firebase';
+import 'firebase/auth';
 
-function App() {
+export const App = () => {
+
+  Firebase.auth().onAuthStateChanged(currentUser => {
+    console.log( currentUser ? 'Esta autenticado' : 'No esta autenticado' );
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola mundo
     </div>
   );
 }
-
-export default App;
---
+++ src/utils/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgUsnatChHfBFCizPKL4MXxzFgNL3PSxQ",
+  authDomain: "musicfy-602d2.firebaseapp.com",
+  databaseURL: "https://musicfy-602d2-default-rtdb.firebaseio.com",
+  projectId: "musicfy-602d2",
+  storageBucket: "musicfy-602d2.appspot.com",
+  messagingSenderId: "10661780625",
+  appId: "1:10661780625:web:7efc2c89f38e8f0ee83330",
+  measurementId: "G-ZMJK8EV9P8"
+};
+
+export default initializeApp(firebaseConfig);
